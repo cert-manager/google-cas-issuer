f73be10b3f8c36aa719e4092e6ac3076a26365f2
joshvanl <me@joshvanl.dev>
2022-08-25 11:25:38 +0100

    Delete the leader election e2e tests
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- test/e2e/suite/suite.go
+++ test/e2e/suite/suite.go
@@ -2,6 +2,5 @@ package suite
 
 import (
 	_ "github.com/jetstack/google-cas-issuer/test/e2e/suite/issuers"
-	_ "github.com/jetstack/google-cas-issuer/test/e2e/suite/leaderelection"
 	_ "github.com/jetstack/google-cas-issuer/test/e2e/suite/validation"
 )
